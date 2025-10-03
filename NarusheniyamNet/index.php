eb362670d1dc68618dbb0f6f807ce9051974a842
kabinet303 <kabinet303@localhost.localdomain>
2025-10-03 09:52:58 +0300

    аааа

--- NarusheniyamNet/index.php
+++ NarusheniyamNet/index.php
@@ -13,8 +13,8 @@
     </header>
 
     <nav>
-        <a href="/DemoExam/Moy_Ne_Sam">Главная</a>
-        <a href="/DemoExam/Moy_Ne_Sam/admin">Админ-панель</a>
+        <a href="/DemoExam/NarusheniyamNet">Главная</a>
+        <a href="/DemoExam/NarusheniyamNet/admin">Админ-панель</a>
     </nav>
 
     <main>    
