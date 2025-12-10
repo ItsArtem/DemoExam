d415e78234a23a029cded5219d972e84a051f248
redos <asdasdas@localhost.localdomain>
2025-12-10 20:53:18 +0300

    Одну букву исправил

--- NarusheniyamNet/struktura.php
+++ NarusheniyamNet/struktura.php
@@ -38,7 +38,7 @@ if (isset($_SESSION['user'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>НарушениямНет - <?php echo $pageTitle; ?></title>
-    <link rel='icon' href='images/logo.jpeg'>
+    <link rel='icon' href='images/logo.jpg'>
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Ваши стили -->
